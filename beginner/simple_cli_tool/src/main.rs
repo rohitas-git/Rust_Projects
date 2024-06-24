24e1bf27a36cb5b2c9e374852fdbeb3c2af2fe9d
rohitas-git <rohitasbansal@icloud.com>
2024-06-24 18:19:44 +0530

    learned to use clap's builder

--- beginner/simple_cli_tool/src/main.rs
+++ beginner/simple_cli_tool/src/main.rs
@@ -1,11 +1,27 @@
-use std::io;
+use clap::{arg, command, value_parser, ArgAction, Command};
+use std::path::PathBuf;
 
 fn main() {
-    println!("Enter your name:");
+    let matches = Command::new("CLI Tool")
+        .version("1.0")
+        .author("Rohit <iamawesome@earth.com>")
+        .about("Performs some operations")
+        .arg(arg!([NAME] "Type your name for greetings").required(false))
+        .arg(arg!(-c --cool [cool] "true for a more cooler greeting").required(false))
+        .get_matches();
 
-    let mut name = String::new();
-    io::stdin().read_line(&mut name)
-        .expect("Failed to read line");
-
-    println!("Hello, {}!", name.trim());
+    if let Some(name) = matches.get_one::<String>("NAME") {
+        let name = name.trim();
+        if let Some(cool) = matches.get_one::<String>("cool"){
+            if cool == "true" {
+                println!("What a amazing day, {}", name);
+            }else if cool == "false" {
+                println!("Chill a bit, {}", name);
+            }else{
+                println!("It's either true or false. Don't be a quantum system!!!");
+            }
+        } 
+    } else {
+        println!("Hello, Rustacean!");
+    }
 }
